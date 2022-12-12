93d1a96a9722d76635d6f66054855e35a5d5fbd1
abdulrazak <razlaw5@gmail.com>
2022-12-12 14:17:01 +0100

    complete the logic for drop zone and add the trash zone delete logic

--- my-stickynote-app/src/App.tsx
+++ my-stickynote-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import StickNote from './components/Stickynote';
 import DropZone from './components/Dropzone';
@@ -6,24 +6,38 @@ import { INote } from './types';
 import './App.css';
 
 function App() {
-const [notes, setNotes] = useState<INote[]>([]);
+  const [notes, setNotes] = useState<INote[]>([]);
 
-const handleCreateNote =()=> {
-const allNotes = [...notes, {
-  id: new Date().getTime(),
-  top: 0,
-  left: 0,
-  content: ""
-}]
+  const handleCreateNote = () => {
+    const newNotes = [...notes, {
+      id: new Date().getTime(),
+      top: 0,
+      left: 0,
+      content: ""
+    }]
+    setNotes(newNotes);
+  }
 
-setNotes(allNotes);
-}
+  const handleDraggedNote = (left: number, top: number, noteId: number) => {
+    const allNotes = [...notes];
+    const note = allNotes.find((n) => n.id === noteId);
+    if (!note) return;
+    note.left = left;
+    note.top = top;
+    setNotes(allNotes);
+  }
+
+  const handleRemovenote = (noteId: number) => {
+    const allNotes = [...notes];
+    const filterNotes = allNotes.filter((n) => n.id !== noteId);
+    setNotes(filterNotes);
+  }
 
   return (
     <React.Fragment>
-      <Navbar createNote={handleCreateNote}/>
-      <DropZone>
-        {notes.map((n)=><StickNote note={n} key={n.id}/>)}
+      <Navbar createNote={handleCreateNote} />
+      <DropZone onDragNote={handleDraggedNote} onRemoveNote={handleRemovenote}>
+        {notes.map((n) => <StickNote note={n} key={n.id} />)}
       </DropZone>
 
     </React.Fragment>
--- my-stickynote-app/src/components/Dropzone/index.tsx
+++ my-stickynote-app/src/components/Dropzone/index.tsx
@@ -1,13 +1,34 @@
+import React from 'react';
 import './dropzone.css';
 interface IDropzoneProps {
     children: React.ReactNode;
+    onDragNote: (newLeft: number, newTop: number, noteId: number ) => void,
+    onRemoveNote: (noteId: number) => void,
 }
 
-const DropZone: React.FC<IDropzoneProps> = ({ children }) => {
+const DropZone: React.FC<IDropzoneProps> = ({ children, onDragNote, onRemoveNote }) => {
+   
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+    }
+
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        const {noteId, offsetX, offsetY} = JSON.parse(event.dataTransfer.getData("text"));
+        //get the actual offset by subtracting the stickynote offset from the parent container offset
+        const x = event.nativeEvent.offsetX - Number(offsetX);
+        const y = event.nativeEvent.offsetY - Number(offsetY);
+        onDragNote(x, y, Number(noteId));
+    }
+
+    const handleRemoveNote = (event: React.DragEvent<HTMLDivElement>) => {
+        const {noteId} = JSON.parse(event.dataTransfer.getData("text"));
+        onRemoveNote(Number(noteId));
+    }
+
     return (
         <div className="dropzone-container">
-            <div className="dropzone">{children}</div>
-            <div className="trashzone">
+            <div className="dropzone" onDrop={(event=>handleDrop(event))} onDragOver={(event=> handleDragOver(event))}>{children}</div>
+            <div className="trashzone" onDrop={(event) => handleRemoveNote(event)} onDragOver={(event) => handleDragOver(event)}>
                 <h2>DRAG NOTE OVER HERE TO REMOVE IT</h2>
             </div>
         </div>
